d27b4d8efde6e7e9549e9340481764376840c532
Ergashev21 <t.ergashev2120@gmail.com>
2026-05-30 09:57:58 +0500

    Prepared

---
+++ Models/Teacher.cs
@@ -0,0 +1,11 @@
+using System.Net.Sockets;
+
+namespace DateBaseOfTeachers.Models;
+public class Teachers
+{
+    public int Id{get; set;}
+    public string FirstName{get; set;}
+    public string LastName{get; set;}
+    public AddressFamily{get; set;}
+    
+}
